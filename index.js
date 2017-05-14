e8e0b9c96eeb8dd7a96ff34d26067903ab2a9021
pravosleva <selection4test@gmail.com>
2017-05-14 15:13:54 +0300

    Send the object instead of the string

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+let app = require('express')(),
+  http = require('http').Server(app),
+  io = require('socket.io')(http),
+  port = process.env.PORT || 3000,
+  clients = [];
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', function(req, res){ res.sendFile(__dirname + '/index.html'); });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  io.emit('chat message', {msg: (socket.id + " connected...")});
+
+  socket.on('chat message', function(obj){
+    io.emit('chat message', obj);
+  });
+
+  socket.on('disconnect', function(){
+    let msg = socket.id + " disconnected...";
+    io.emit('chat message', { msg: msg } );
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+http.listen(port, function(){ console.log('listening on *:' + port); });
